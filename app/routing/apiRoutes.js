0da267a06cfc0b2634fb9d4d250e332d884082e9
Peter Norman <october8th@hotmail.com>
2018-02-04 20:59:41 -0500

    pushing the app to heroku

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,35 +10,32 @@ module.exports = function(app)
 
     app.post("/api/submitSurvey", function(req,res) 
     {
-   		console.log(req.body);
+   		//console.log(req.body);
    		//console.log(JSON.parse(req.body));
     	var newMatch = req.body;
     	var matchDelta = 500;
     	var myMuppet = {};
+    	var myMuppetIndex = 0;
     	//console.log("json stringify: " + JSON.stringify(newMatch));
     	//console.log("type of: " + typeof newMatch);
-    	console.log("length of my matches:" + muppets.length);
+    	//console.log("length of my matches:" + muppets.length);
         for (var i=0; i<muppets.length; i++)
         {
         	var newDelta = 0;
-        	//console.log(JSON.parse(newMatch))
-        	console.log(muppets[0].scores.length);
-        	console.log(muppets[0].scores[0]);
-        	console.log(newMatch.name);
-        	console.log(newMatch.photo);
-        	console.log(Object.keys(newMatch));
-        	console.log(newMatch.scores[0]);
-        	
       		for (var j=0; j < muppets[i].scores.length; j++)
       		{
          		newDelta += (Math.abs(parseInt(newMatch.scores[j])-parseInt(muppets[i].scores[j])));
       		}
       		if (newDelta < matchDelta)
       		{
+      			matchDelta = newDelta;
       			myMuppet = muppets[i];
+      			myMuppetIndex = i;
       		}
      	}
-     	muppets[i].matches.push([newMatch.name,newMatch.photo]);
-     	return(myMuppet);
+     	muppets[myMuppetIndex].matches.push([newMatch.name,newMatch.photo]);
+     	//console.log(muppets[myMuppetIndex]);
+     	//res.json(true);
+     	res.json(muppets[myMuppetIndex]);
    });
 };
